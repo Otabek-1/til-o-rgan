3c9b2b4ceaba3bc808c398e8b1015b8d21e85674
Otabek <105920915+Otabek-1@users.noreply.github.com>
2025-06-02 17:33:55 +0500

    Update index.js

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ async function sendAsTest(userId) {
 
   await bot.telegram.sendMessage(
     userId,
-    `👉 ${wordEn} so‘zining tarjimasini tanlang:`,
+    `👉 ${wordEn} so‘zining tarjimasini toping:`,
     {
       reply_markup: {
         inline_keyboard: [buttons],
@@ -186,7 +186,7 @@ async function handleStart(ctx) {
     if (user.rows.length === 0) {
       await pool.query(
         "INSERT INTO users (telegram_id, remembering_words, schedule) VALUES ($1, $2, $3)",
-        [userId, [], 30] // Default: 30 daqiqa
+        [userId, [], 2] // Default: 30 daqiqa
       );
       await ctx.reply("✅ Xush kelibsiz! So'z qo'shish uchun /add ni bosing.");
     } else {
